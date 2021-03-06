5a417bed846279e5811df7d69f9af0a1a166045c
Cristiano Chelotti <crisc98@gmail.com>
2021-03-06 14:20:59 -0500

    Main activity code cleanup

--- app/src/main/java/com/fydp/signsenseclient/MainActivity.java
+++ app/src/main/java/com/fydp/signsenseclient/MainActivity.java
@@ -1,60 +1,14 @@
 package com.fydp.signsenseclient;
 
 import android.content.Intent;
-import android.graphics.SurfaceTexture;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.SurfaceView;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmarkList;
-import com.google.mediapipe.components.CameraHelper;
-import com.google.mediapipe.components.CameraXPreviewHelper;
-import com.google.mediapipe.components.ExternalTextureConverter;
-import com.google.mediapipe.components.FrameProcessor;
-import com.google.mediapipe.glutil.EglManager;
-import java.util.HashMap;
+
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-    private static final String BINARY_GRAPH_NAME = "holistic_tracking_gpu.binarypb";
-    private static final String INPUT_VIDEO_STREAM_NAME = "input_video";
-    private static final String OUTPUT_VIDEO_STREAM_NAME = "output_video";
-    private static final String OUTPUT_LHAND_LANDMARKS_STREAM_NAME = "left_hand_landmarks";
-    private static final String OUTPUT_RHAND_LANDMARKS_STREAM_NAME = "right_hand_landmarks";
-    private static final String OUTPUT_POSE_LANDMARKS_STREAM_NAME = "pose_landmarks";
-    private static final CameraHelper.CameraFacing CAMERA_FACING = CameraHelper.CameraFacing.FRONT;
-    private HashMap<String, NormalizedLandmarkList> landmarkLists;
-    private ClientSend clientSocket;
-    // Flips the camera-preview frames vertically before sending them into FrameProcessor to be
-    // processed in a MediaPipe graph, and flips the processed frames back when they are displayed.
-    // This is needed because OpenGL represents images assuming the image origin is at the bottom-left
-    // corner, whereas MediaPipe in general assumes the image origin is at top-left.
-    private static final boolean FLIP_FRAMES_VERTICALLY = true;
-    static {
-        // Load all native libraries needed by the app.
-        System.loadLibrary("mediapipe_jni");
-        System.loadLibrary("opencv_java3");
-    }
-    // {@link SurfaceTexture} where the camera-preview frames can be accessed.
-    private SurfaceTexture previewFrameTexture;
-    // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
-    private SurfaceView previewDisplayView;
-    // Creates and manages an {@link EGLContext}.
-    private EglManager eglManager;
-    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
-    // frames onto a {@link Surface}.
-    private FrameProcessor processor;
-    // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
-    // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
-    private ExternalTextureConverter converter;
-    // Handles camera access via the {@link CameraX} Jetpack support library.
-    private CameraXPreviewHelper cameraHelper;
-
-    private Button startButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
 
-        startButton = findViewById(R.id.startButton);
+        Button startButton = findViewById(R.id.startButton);
         startButton.setOnClickListener(v -> {
             startStreaming();
         });
     }
     private void startStreaming() {
-        Intent myIntent = new Intent(MainActivity.this, StreamActivity.class);
-        MainActivity.this.startActivity(myIntent);
+        Intent streamIntent = new Intent(MainActivity.this, StreamActivity.class);
+        MainActivity.this.startActivity(streamIntent);
     }
 }
